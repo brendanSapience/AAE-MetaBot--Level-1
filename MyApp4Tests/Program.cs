2433a531bab0b1b8752315f71b5eb47fed54a940
brendan sapience <brendan.sapience@gmail.com>
2019-01-27 01:23:27 +0000

    release

---
+++ MyApp4Tests/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyApp4Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp4Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VisibleBotMethods vbm = new VisibleBotMethods();
+            String Message1 = vbm.SayHiTo("Bren");
+            String Message2 = vbm.SayByeTo("Bren");
+            Console.WriteLine("Here is your Message: " + Message1);
+            Console.WriteLine("Here is your other Message: " + Message2);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ MyAppLib/VisibleBotMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp4Lib
+{
+    public class VisibleBotMethods
+    {
+        public String SayHiTo(String Name)
+        {
+            String MyMessage = "Hi There, " + Name;
+            return MyMessage;
+        }
+
+        public String SayByeTo(String Name)
+        {
+            String MyMessage = "Goodbye " + Name;
+            return MyMessage;
+        }
+    }
+}
